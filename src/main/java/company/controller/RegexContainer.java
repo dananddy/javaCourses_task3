7a6fdf41c24ed30e891901cee96fcfa2984506d8
dan_anddy <taorus.warior@gmail.com>
2020-02-16 19:36:44 +0200

    configured view inteface and regex for name , login

---
+++ src/main/java/company/controller/RegexContainer.java
@@ -0,0 +1,10 @@
+package company.controller;
+
+public class RegexContainer {
+
+    String REGEX_NAME_UKR = "^[А-ЩЬЮЯҐІЇЄ][а-щьюяґіїє']{1,20}$";
+    String REGEX_NAME_LAT = "^[A-Z][a-z]{1,20}$";
+    String REGEX_LOGIN = "^[A-Za-z0-9_-]{8,20}$";
+    String REGEX_PHONE_NUMBER = "";
+
+}
---
+++ src/main/java/company/view/TetConstant.java
@@ -0,0 +1,8 @@
+package company.view;
+
+public interface TetConstant {
+    String INPUT_STRING_DATA = "input.data";
+    String FIRST_NAME = "input.first.name";
+    String WRONG_INPUT_DATA = "wrong.input";
+    String LOGIN_DATA = "input.login.";
+}
